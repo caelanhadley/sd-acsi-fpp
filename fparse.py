84987ce17b2e6aaa0aec03c372c68da1e177e739
Caelan Hadley <clh0089@auburn.edu>
2023-10-22 14:38:18 -0500

    Added commend handling

--- fparse.py
+++ fparse.py
@@ -30,55 +30,17 @@ def get_input():
     with open("./port.fpp", 'r') as f:
         return f.read()
 
-'''
-Tokenizer
-
-'''
-def tokenizer(input, reserved_words, valid_symbols):
-    tokens = []
-    token_buffer = ""
-    comment_flag = 0
-    newline_flag = 0
-
-    for c in input:
-        if c == '#'or c == "@": comment_flag = 1 # note: Consider moving @ comments into a different area so they can be stored
-        if comment_flag:
-            if c == '\n': comment_flag = 0
-            else: continue
-        if (c != ' ' and c != '\n'):
-            token_buffer += c
-        elif (token_buffer != ""):
-            tokens.append(token_buffer)
-            token_buffer = ''
-    return tokens
-
-def parser(tokens):
-    index = 0
-    while index < len(tokens):
-        if tokens[index] == 'port':
-            print(f"PORT => {tokens[index - 2]} {tokens[index - 1]} {tokens[index]} {tokens[index + 1]}")
-        index += 1
-
-
-
-def temp(reserved_words, token_buffer):
-    match_buffer = "NONE"
-    for word in reserved_words:
-        if token_buffer == word:
-            match_buffer = word
 def main():
     # Initializing vars
     valid_symbols = mount_symbols()
     reserved_words = mount_reserved()
     
     # Tokenize File
-    # tokens = tokenizer(get_input(), reserved_words, valid_symbols)
     tokenizer = Tokenizer(get_input())
 
 '''
 Parameter
-    FORMAT
-    [ ref ] identifier : type-name
+    FORMAT -> [ ref ] identifier : type-name
 '''
 class Parameter:
     TYPE = 'parameter'
@@ -88,15 +50,19 @@ class Parameter:
 
 '''
 Port
-    FORMAT
-    port identifier [ ( param-list ) ] [ -> type-name ]
+    FORMAT -> port identifier [ ( param-list ) ] [ -> type-name ]
 '''
 class Port:
+    TYPE = 'port'
     def __init__(self):
         self.identifier = ''
         self.param_list = [] # Type : <Parameter>
         self.type_name = ''
 
+'''
+Port Instance Specifier
+
+'''
 class PortInstanceSpecifier:
     VALID_INPUT_KIND = ['async', 'guarded', 'sync'] # Requires the input tag to follow it
     VALID_OUTPUT_KIND = ['output']
@@ -111,7 +77,12 @@ class PortInstanceSpecifier:
         self.queue_full_behavior = queue_full_behavior # <assert, block, drop>
         self.special_port_kind = special_port_kind
         self.priority = priority # Must be convertable to Integer
+        self.description = '' # Description from "@" comments
 
+'''
+Tokenizer class
+
+'''
 class Tokenizer:
     def __init__(self, input):
         self.input = input
@@ -120,8 +91,7 @@ class Tokenizer:
         self.tokenize()
     
     # Saves the contents of the buffer
-    def save(self):
-        print(self.buffer)
+    def save_buffer(self):
         self.tokens.append(self.buffer)
         self.clbuf()
 
@@ -138,23 +108,33 @@ class Tokenizer:
         while i < len(self.input):
             # If this is a space save whats in the buffer
             if self.input[i] == ' ' and not self.isEmpty():
-                self.save()
+                self.save_buffer()
                 i += 1
             else:
                 if self.input[i] == '\n' and i+1 < len(self.input):
                     # If there is something in the buffer save it
                     if not self.isEmpty():
-                        self.save()
+                        self.save_buffer()
                     # Parse until no more newlines or spaces
                     while self.input[i] == '\n' or self.input[i] == ' ':
                         i += 1
                     self.tokens.append("\n")
+                # Handles Hashtag Comments
+                elif self.input[i] == '#' and i+1 < len(self.input):
+                    while self.input[i] != '\n':
+                        i += 1
+                # Handles Description Comments
+                elif self.input[i] == '@' and i+1 < len(self.input):
+                    comment_buffer = ''
+                    while self.input[i] != '\n':
+                        comment_buffer += self.input[i]
+                        i += 1
                 else:
                     self.buffer += self.input[i]
                     i += 1
         if not self.isEmpty():
-            self.save()
-        self.parse()
+            self.save_buffer()
+        self.printBuffer()
     
     def parse(self):
         i = 0
@@ -165,6 +145,10 @@ class Tokenizer:
                 elif self.tokens[i-1] == 'output':
                     port = PortInstanceSpecifier(general_port_kind=(self.tokens[i-1]))
             i+=1
+    
+    def printBuffer(self):
+        for token in self.tokens:
+            print(token)
 
 
 # Init
